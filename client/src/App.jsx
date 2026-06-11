9ff7dba17eb694a096b58bf0f4b8bf59dfa00859
Harsha125-art <harshavardhani.d24@iiits.in>
2026-06-11 13:02:30 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+         <Route path="/" element={<Login />} />
         <Route
           path="/dashboard"
           element={
